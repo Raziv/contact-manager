d48aa145a745b92204d0fc1be33660d6b0f0cfa3
Rajiv karki <rajiv.karki@outlook.com>
2018-02-06 17:28:16 -0500

    add user model validations with password digest column

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'users#new'
+
+  get '/signup', to: 'users#new'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206181538) do
+ActiveRecord::Schema.define(version: 20180206221234) do
 
   create_table "contacts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20180206181538) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "contacts", "groups"
