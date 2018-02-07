55408889442cf8236a758134a3e90e01e4dc9743
Rajiv karki <rajiv.karki@outlook.com>
2018-02-07 11:33:24 -0500

    Contacts, groups controller, model files

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,14 @@
 class Contact < ApplicationRecord
   belongs_to :user
-  belongs_to :group
+  belongs_to :group, optional: true
+
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum:255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true,
+                    case_sensitive: false
+  validates :phone, presence: true, numericality: true,
+            length: { minimum: 10, maximum: 15 }
 end
